f3be195e8972c072f25019f15434fc1b3d48a55e
DESKTOP-MVF7U6O\marco <marco3.turetta@mail.polimi.it>
2019-04-02 23:30:12 +0200

    aggiunta dataset+plotting via qgis

---
+++ Implementation/first_plots.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Apr  2 22:48:44 2019
+
+@author: marco
+"""
+
+import plotly.plotly as py
+import plotly.graph_objs as go
+
+import pandas as pd
+
+df_como=pd.read_csv('./Datasets/Como_dataset.csv')
+print(df_como.head())
